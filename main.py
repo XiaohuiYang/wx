0d137e5de0fce2e4bb987ef7819048b92478c467
XiaohuiYang <yxh.buaa@gmail.com>
2016-01-16 18:54:15 +0800

    修改配置读取到错误

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from crawler import run
 cf = ConfigParser.ConfigParser()
  
 cf.read("config.conf")
-names = cf.get("wx", "names")
+names = cf.get("wx", "names").split(',')
 
 logger = mylog.logger
 
@@ -26,25 +26,25 @@ logger = mylog.logger
 # logger.addHandler(fh)
 
 counter = 0;
-# print names[counter]
+print names[counter].strip()
 
 def crawler():
     global counter 
     counter += 1
     counter = counter % len(names)
     # print 'aaa'
-    print names[counter]
-    logger.info("启动casper抓取账号" + names[counter]+ " at "+ time.ctime())
+    print names[counter].strip()
+    logger.info("启动casper抓取账号" + names[counter].strip()+ " at "+ time.ctime())
     CASPERJS_EXECUTABLE = "casperjs" # <-- here you put the path to you casperjs executable
     CASPERJS_SCRIPT = "./js/dy_crawler.js" # this is the name of the script that casperjs should execute
-    stdout_as_string = subprocess.Popen([CASPERJS_EXECUTABLE, CASPERJS_SCRIPT, names[counter]])
+    stdout_as_string = subprocess.Popen([CASPERJS_EXECUTABLE, CASPERJS_SCRIPT, names[counter].strip()])
     # print 'bbb'
     # print stdout_as_string
 def analysis():
     logger.info("开始分析" + time.ctime())
     run()
 
-schedule.every(10).minutes.do(crawler)
+schedule.every(10).seconds.do(crawler)
 schedule.every().day.at("15:06").do(analysis)
 
 while True:
